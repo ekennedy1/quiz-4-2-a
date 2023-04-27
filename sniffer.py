b9184ac49e506cac6ccbeda053ed67cad1e3b245
ekennedy1 <ekennedy@tamu.edu>
2023-04-27 23:00:35 +0000

    push

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,6 @@
 from scapy.all import *
+from scapy.all import Ether, IP, TCP, sendp
+import time
 
 
 def main():
@@ -9,15 +11,21 @@ def main():
         if option == '1':
             print("Creating and sending packets ...")
             # TODO
+            number = input('How many packets? ')
+            interval = input('How many seconds in between sending? ')
+            send_pkt(number, interval)
         elif option == '2':
             print("Listening to all traffic to 8.8.4.4 for 1 minute ...")
             # TODO
+            sniff(filter="dst 8.8.4.4", prn=print_pkt, timeout=60)
         elif option == '3':
             print("Listening continuously to only ping commands to 8.8.4.4 ...")
             # TODO
+            sniff(filter="icmp and dst 8.8.4.4", prn=print_pkt, timeout=60)
         elif option == '4':
             print("Listening continuously to only outgoing telnet commands ...")
             # TODO
+            sniff(filter="dst port 23", prn=print_pkt, timeout=60)
         elif option == '5':
             print("End")
             break
@@ -48,6 +56,23 @@ def send_pkt(number, interval):
     """
 
     # TODO
+    # Define the Ethernet frame
+    eth = Ether(src='00:11:22:33:44:55', dst='55:44:33:22:11:00', type=0x0800)
+
+    # Define the IP packet
+    ip = IP(src='192.168.10.4', dst='8.8.4.4', proto='tcp', ttl = 26)
+
+    # Define the TCP segment
+    tcp = TCP(sport=23, dport=80)
+
+    # Define the payload
+    payload = b'RISC-V Education: https://riscvedu.org/'
+
+    # Combine everything into a single packet
+    packet = eth/ip/tcp/payload
+
+    sendp(packet, count=int(number), inter=int(interval))
+
     pass
 
 
@@ -64,6 +89,16 @@ def print_pkt(packet):
     """
 
     # TODO
+    
+    print("Source IP:", packet[IP].src)
+    print("Destination IP:", packet[IP].dst)
+    print("Protocol:", packet[IP].proto)
+    print("Packet TTL:", packet[IP].ttl)
+    print("Packet Length:", packet[IP].len)
+    print("Packet Payload:", packet[IP].load)    
+    ##print(f"The packet full data is {packet.show()}")
+    print("\n")
+    
     pass
 
 
